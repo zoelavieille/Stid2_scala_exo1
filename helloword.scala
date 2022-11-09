f8d0a4ac1d790aec5c19de4630d4ad917fc37dce
zoe lavieille <zoe.lavieille@etu.univ-poitiers.fr>
2022-11-09 11:12:02 +0000

    first version

---
+++ helloword.scala
@@ -0,0 +1,16 @@
+
+object Geeks 
+{
+    // Main Method 
+    def main(args: Array[String])= 
+    { 
+         var var3=3
+	if(var3==1){
+        // prints Hello World
+	print("TRUE")
+	}else
+	{
+        println("FALSE") 
+    }
+}
+}
